90b4ed11b4491fe49c5bccad6a56a0aa64d1671b
Pravnan <94179778+Pravnan@users.noreply.github.com>
2024-07-25 22:15:22 +0530

    update 2.1

--- toystore/src/app/components/card-set1/card-set1.jsx
+++ toystore/src/app/components/card-set1/card-set1.jsx
@@ -1,16 +1,17 @@
-// toystore/src/app/components/card-set1/card-set1.jsx
-
-import React, { useEffect, useState } from 'react';
-import './card-set1.css'; // Import the new CSS file
+import React, { useRef, useEffect, useState } from 'react';
+import './card-set1.css'; // Import the CSS file
 
 const CardSet1 = () => {
   const [cards, setCards] = useState([]);
+  const cardWrapperRef = useRef(null);
+  const scrollDirection = useRef('right'); // Ref to track scroll direction
+  const widthToScroll = useRef(0);
+  const autoScrollInterval = useRef(null);
 
   useEffect(() => {
     // Function to fetch JSON data and generate cards
     const fetchCards = async () => {
       try {
-        // Fetch the JSON file from the public directory
         const response = await fetch('/json/cards.json');
         const cardsData = await response.json();
         setCards(cardsData);
@@ -22,31 +23,30 @@ const CardSet1 = () => {
     fetchCards();
   }, []);
 
+  
+
   return (
-    <div className="card-container">
-      <h1>Featured Products ➜</h1>
-        <i className="fa-solid fa-arrow-right"></i>
-
-      <div className="card-set">
-        {cards.map((card) => (
-          <div key={card.id} className="card">
-            <img src={card.imageUrl} className="card-img-top" alt={card.title} />
-            <div className="card-body">
-              <h5 className="card-title">{card.title}</h5>
-              <p className="card-price">{card.price}</p>
-              {/* <p className="card-price2">{card.price2}</p> Display the price */}
-              {/* <div className="buttons"> */}
-              <div class="buttons">
-                <a href="your-link-here" class="add-cart"><span>Add to Cart</span></a>
-                </div>
-
-              {/* <a href="#" className="add-fav">Favorites</a> */}
-              {/* </div> */}
+      <div className="card-container">
+        <h1>Featured Products</h1>
+          <i className="fa-solid fa-arrow-right"></i>
+  
+        <div className="card-set">
+          {cards.map((card) => (
+            <div key={card.id} className="card">
+              <img src={card.imageUrl} className="card-img-top" alt={card.title} />
+              <div className="card-body">
+                <h5 className="card-title">{card.title}</h5>
+                <p className="card-price">{card.price}</p>
+                <div class="buttons">
+                  <a href="your-link-here" class="add-cart"><span>Add to Cart</span></a>
+                  </div>
+  
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+  
   );
 };
 
